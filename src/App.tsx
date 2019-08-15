7a4874bdcaaabe3cccecb6a04393b97fcddfceee
Alex <alxford45@gmail.com>
2019-08-14 19:38:41 -0500

    defined onClick function to increment count

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const App: React.FC = () => {
   const [count, setCount] = React.useState(0);
+  const onClick = () => setCount(x => x + 1);
   return (
     <React.Fragment>
       <div>count</div>
