3a03776160410996d6853eede7b5ea486320b7c2
Alex <alxford45@gmail.com>
2019-08-14 19:34:10 -0500

    added dummy count, wrapped tsx in Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-const App: React.FC = () => <button>click me!</button>;
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div>count</div>
+      <button>click me!</button>);
+    </React.Fragment>
+  );
+};
 
 export default App;
